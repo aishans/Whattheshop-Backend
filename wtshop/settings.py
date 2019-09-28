37350e58c3c38c865b87d14230a1da9f140d02a3
aishans <alshaijiaisha@gmail.com>
2019-09-28 18:41:08 +0300

    data

--- wtshop/settings.py
+++ wtshop/settings.py
@@ -108,9 +108,13 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+   'DEFAULT_AUTHENTICATION_CLASSES': (
        'rest_framework.authentication.TokenAuthentication',
    ),
-  
+   'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAdminUser'
+   ),
 }
 
 
